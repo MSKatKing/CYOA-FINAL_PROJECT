7a61323219fc3c62c97b6c19ee7faf231ca7b22c
MSKatKing <mark.schaefges08@gmail.com>
2022-12-19 17:30:57 -0600

    terrible story code

--- scripts/enemies.py
+++
@@ -1,7 +0,0 @@
-import random
-import resources
-import pygame
-
-class Wolf(resources.Entity):
-    def __init__(self):
-        super().__init__(pygame.transform.scale(pygame.image.load("resources/sun.png"), (10, 10)), (10, 10), 10, 12)
